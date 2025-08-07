261c9560bde33acb809b65467dda5037503467f8
moshik <moshik0527669169@gmail.com>
2025-08-07 15:11:29 +0300

    created server

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/submit', routerSubmit);
+
+app.post('/admin', routerAdmin);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

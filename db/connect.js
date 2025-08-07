370ba8e4280eaf07de1294a9119470354f0ca31e
moshik <moshik0527669169@gmail.com>
2025-08-07 16:27:26 +0300

    x

--- db/connect.js
+++ db/connect.js
@@ -11,7 +11,6 @@ export async function connectToMongo(){
         console.log(error)
     }
 }
-
 export const db = client.db('complains-project')
 
 
--- routes/routerSubmit.js
+++ routes/routerSubmit.js
@@ -3,8 +3,7 @@ const routerSubmit = express.Router();
 
 routerSubmit.post('/', (req, res) => {
   const { domain, message } = req.body;
-
-  res.send('<h1>Thank you for your complaint!</h1><a href="/">Back</a>');
+  res.send(`<h1>Thank you for your complaint!</h1><a href="/">Back</a>`);
 });
 
 export default routerSubmit;
